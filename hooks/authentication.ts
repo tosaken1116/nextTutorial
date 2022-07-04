a43dbf43def4bfd414b05084447c803af941726a
tosaken1116 <nagato20021122@gmail.com>
2022-07-05 01:14:24 +0900

    bootstrapによるデザインの実装

--- hooks/authentication.ts
+++ hooks/authentication.ts
@@ -2,16 +2,34 @@ import { getAuth, onAuthStateChanged, signInAnonymously } from 'firebase/auth'
 import { useEffect } from 'react'
 import { atom, useRecoilState } from 'recoil'
 import { User } from '../models/User'
-
+import { 
+  getFirestore,
+  collection,
+  doc,
+  getDoc,
+  setDoc,
+} from 'firebase/firestore'
 const userState = atom<User>({
   key: 'user',
   default:null,
 })
 
+async function createUserIfNotFound(user:User){
+  const db = getFirestore()
+  const usersCollection = collection(db,'users')
+  const userRef = doc(usersCollection,user.uid)
+  const document = await getDoc(userRef)
+  if(document.exists()){
+    return
+  }
+
+  await setDoc(userRef,{
+    name:'taro' + new Date().getTime(),
+  })
+}
+
 export function useAuthentication() {
   const[user,setUser] = useRecoilState(userState)
-
-
 
   useEffect(()=>{
     if(user !== null){
@@ -29,10 +47,13 @@ export function useAuthentication() {
     onAuthStateChanged(auth, function (firebaseUser) {
       if (firebaseUser) {
         console.log('set user')
-        setUser({
-          uid: firebaseUser.uid,
+        const loginUser:User = {
+          uid:firebaseUser.uid,
           isAnonymous: firebaseUser.isAnonymous,
-        })
+          name:'',
+        }
+        setUser(loginUser)
+        createUserIfNotFound(loginUser)
       } else {
         // User is signed out.
         setUser(null)
---
+++ pages/users/[uid].tsx
@@ -0,0 +1,48 @@
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { User } from '../../models/User'
+import {collection,doc,getDoc,getFirestore} from 'firebase/firestore'
+import Layout from '../../components/Layout'
+type Query ={
+    uid:string
+}
+
+export default function UserShow() {
+    const [user,setUser] = useState<User>(null)
+    const router = useRouter()
+    const query = router.query as Query
+
+    useEffect(() =>{
+        if(query.uid === undefined){
+            return
+        }
+        async function loadUser(){
+            console.log(query)
+            const db = getFirestore()
+            const ref = doc(collection(db,'users'),query.uid)
+            const userDoc = await getDoc(ref)
+
+            if(!userDoc.exists()){
+                console.log('returned')
+                return
+            }
+
+            const gotUser = userDoc.data() as User
+            gotUser.uid = userDoc.id
+            setUser(gotUser)
+        }
+        loadUser()
+    },[query.uid])
+
+    return (
+        <Layout>
+            {user &&(
+                <div className="text-center">
+                    <h1 className="h4">{user.name}さんのページ</h1>
+                    <div className="m-5">{user.name}さんに質問しよう</div>
+                </div>
+            )}
+        </Layout>
+    )
+}
